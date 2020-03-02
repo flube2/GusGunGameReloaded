c75c1245a975db53f522939a3756a42503d94de0
Frank Lubek <flubek@gmail.com>
2020-03-02 01:49:30 -0600

    Swap most functionality from GameMode, etc. to Level BP's and PlayerState etc., need new HUD for L1 onwards

---
+++ Source/GusGunGameReloaded/GusGunGameReloaded.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GusGunGameReloaded.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GusGunGameReloaded, "GusGunGameReloaded" );
